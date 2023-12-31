c4a5b88a51e2e1a5d8c0e5a4dadda3608e9993a7
t33y <omotayoolarewaju@gmail.com>
2023-12-31 18:17:14 +0100

    creates local storage logic no user auth

--- src/auth.ts
+++ src/auth.ts
@@ -1,56 +1,56 @@
-import { MongoDBAdapter } from "@next-auth/mongodb-adapter";
-import { MongoClient } from "mongodb";
-import { AuthOptions, getServerSession } from "next-auth";
-import CredentialsProvider from "next-auth/providers/credentials";
-import GoogleProvider from "next-auth/providers/google";
-import clientPromise from "./mongodb";
+// import { MongoDBAdapter } from "@next-auth/mongodb-adapter";
+// import { MongoClient } from "mongodb";
+// import { AuthOptions, getServerSession } from "next-auth";
+// import CredentialsProvider from "next-auth/providers/credentials";
+// import GoogleProvider from "next-auth/providers/google";
+// import clientPromise from "./mongodb";
 
-export const authOptions: AuthOptions = {
-  providers: [
-    GoogleProvider({
-      clientId: process.env.GOOGLE_CLIENT_ID as string,
-      clientSecret: process.env.GOOGLE_CLIENT_SECRET as string,
-    }),
-    CredentialsProvider({
-      name: "email and password",
-      credentials: {
-        email: {
-          label: "Email",
-          placeholder: "email@email.com",
-          type: "email",
-        },
-        password: { label: "Password", type: "password" },
-      },
-      async authorize(credentials, req) {
-        if (!credentials?.email || !credentials?.password) {
-          throw new Error("Please provide valid credentials");
-        }
+// export const authOptions: AuthOptions = {
+//   providers: [
+//     GoogleProvider({
+//       clientId: process.env.GOOGLE_CLIENT_ID as string,
+//       clientSecret: process.env.GOOGLE_CLIENT_SECRET as string,
+//     }),
+//     CredentialsProvider({
+//       name: "email and password",
+//       credentials: {
+//         email: {
+//           label: "Email",
+//           placeholder: "email@email.com",
+//           type: "email",
+//         },
+//         password: { label: "Password", type: "password" },
+//       },
+//       async authorize(credentials, req) {
+//         if (!credentials?.email || !credentials?.password) {
+//           throw new Error("Please provide valid credentials");
+//         }
 
-        const client = await MongoClient.connect(
-          process.env.MONGODB_URI as string
-        );
+//         const client = await MongoClient.connect(
+//           process.env.MONGODB_URI as string
+//         );
 
-        const db = client.db();
+//         const db = client.db();
 
-        const user = await await db
-          .collection("users")
-          .findOne({ email: credentials.email });
+//         const user = await await db
+//           .collection("users")
+//           .findOne({ email: credentials.email });
 
-        if (user) {
-          return user as any;
-        } else {
-          return null;
-        }
-      },
-    }),
-  ],
-  session: {
-    strategy: "jwt",
-  },
-  adapter: MongoDBAdapter(clientPromise),
-  debug: process.env.NODE_ENV === "development",
-  secret: process.env.NEXTAUTH_SECRET,
-};
-export const getAuthSession = () => {
-  return getServerSession(authOptions);
-};
+//         if (user) {
+//           return user as any;
+//         } else {
+//           return null;
+//         }
+//       },
+//     }),
+//   ],
+//   session: {
+//     strategy: "jwt",
+//   },
+//   adapter: MongoDBAdapter(clientPromise),
+//   debug: process.env.NODE_ENV === "development",
+//   secret: process.env.NEXTAUTH_SECRET,
+// };
+// export const getAuthSession = () => {
+//   return getServerSession(authOptions);
+// };
