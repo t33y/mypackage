c36758b54481635eb651bdc0e795b28deedb2d12
t33y <omotayoolarewaju@gmail.com>
2023-12-31 18:34:05 +0100

    removes nextauth installation from local storage logic

--- src/trpc/trpc.ts
+++ src/trpc/trpc.ts
@@ -1,12 +1,11 @@
-import { getAuthSession } from "@/auth";
+// import { getAuthSession } from "@/auth";
 import { initTRPC } from "@trpc/server";
 
 export const createTRPCContext = async () => {
-  const session = await getAuthSession();
-
-  return {
-    session,
-  };
+  // const session = await getAuthSession();
+  // return {
+  //   session,
+  // };
 };
 
 const t = initTRPC.context<typeof createTRPCContext>().create();
@@ -14,16 +13,16 @@ const t = initTRPC.context<typeof createTRPCContext>().create();
 export const router = t.router;
 export const publicProcedure = t.procedure;
 
-const enforceUserIsAuthed = t.middleware(({ ctx, next }) => {
-  if (!ctx.session || !ctx.session.user) {
-    throw new Error("UNAUTHORIZED");
-  }
-  return next({
-    ctx: {
-      // infers the `session` as non-nullable
-      session: { ...ctx.session, user: ctx.session.user },
-    },
-  });
-});
+// const enforceUserIsAuthed = t.middleware(({ ctx, next }) => {
+//   if (!ctx.session || !ctx.session.user) {
+//     throw new Error("UNAUTHORIZED");
+//   }
+//   return next({
+//     ctx: {
+//       // infers the `session` as non-nullable
+//       session: { ...ctx.session, user: ctx.session.user },
+//     },
+//   });
+// });
 
-export const protectedProcedure = t.procedure.use(enforceUserIsAuthed);
+// export const protectedProcedure = t.procedure.use(enforceUserIsAuthed);
