5a9625e52ecd39e522f7d9d38779b24d3b81f2df
t33y <omotayoolarewaju@gmail.com>
2024-01-20 11:29:24 +0100

    Logs dispatcher location

--- src/components/providers/DispatcherLocation.tsx
+++ src/components/providers/DispatcherLocation.tsx
@@ -37,5 +37,6 @@ export const useDispatcherLocationContext = () => {
       "Please ensure component is within the DispatcherLocationProvider"
     );
   }
+  console.log(context);
   return context;
 };
